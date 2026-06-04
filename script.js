9925369b1f25b8983d1ae624a48163d5a6ea431a
Juan <juan.barrantesloria@gmail.com>
2026-06-04 01:22:23 -0600

    Feature:funcionatodo_menoslosdecimales

--- script.js
+++ script.js
@@ -1,75 +1,72 @@
 
-let numero; //El valor numerico
-let operador; //Esto para saber la operacion a realizar
+let numero = 0; //El valor numerico
+let operador = ''; //Esto para saber la operacion a realizar
 const terminosOperacion = []; //Solo puede haber maximo 2 ya que son los que se van a operar
-let flagOperacion = false; //Para saber si es termino uno o termino dos, 
-// ya que antes del dos, tuvo que tocar el btn de alguna operacion
 
 document.addEventListener('DOMContentLoaded', () => {
     const barraResultado = document.getElementById('areaResultados');
     document.querySelectorAll('button[id*="numeros_btn"]').forEach(btn => {
         btn.addEventListener('click', evento => {
-            if (evento.target.textContent == '0') {
-                if (barraResultado.textContent == '') {
-                    return; //Para que no agregue nada si empieza el numero con 0
-                }
-            }
             barraResultado.value += evento.target.textContent;
         });
     });
 
     document.querySelectorAll('button[id*="operacion_btn"]').forEach(btn => {
         btn.addEventListener('click', evento => {
-            const valores = barraResultado.value.split('');
-            //console.log('valores es:' + valores);
-            numero = ((valores) => {
-                let num = ''; //Para construir el numero primero en string y luego lo retorno en num
-                valores.forEach(valor => {
-                    num += valor;
-                });
-                return Number(num);
-            })(valores);
-            if (!flagOperacion) {
-                terminosOperacion[0] = numero;
-                flagOperacion = true;
+            
+            if(terminosOperacion.length == 0){
+                terminosOperacion[0] = construirNumero(barraResultado.value);
+                operador = evento.target.textContent;
+                barraResultado.value = '';
             }
-            //console.log('El numero e:' + numero);
-            //console.log(terminosOperacion);
-            operador = evento.target.textContent;
-            barraResultado.value = '';
-            //numero = 0;
+            else{
+                document.getElementById('btnResolver').click();
+            }
         });
     });
-    
 
     document.getElementById('btnLimpiar').addEventListener('click', evento => {
         barraResultado.value = '';
-        flagOperacion = false;
         terminosOperacion.length = 0;
         operador = '';
+        numero = 0;
     });
 
-    document.getElementById('btnResolver').addEventListener('click', () => {
-        const resultado;
-        switch(operador){
-            case '+':
-                terminosOperacion[1] = numero;
-                numero =0;
-                
-                operador = '';
-                break;
-            case '-':
-
-                break;
-            case '/':
-
-                break;
-            case '*':
-
-                break;
-            default:
-                alert('Operacion no valida');
-                break;
+    document.getElementById("btnResolver").addEventListener('click', () => {
+        let resultado;
+        if(terminosOperacion.length == 0){
+            document.getElementById('btnLimpiar').click();
+        }
+        else if(terminosOperacion.length == 1){
+            terminosOperacion[1] = construirNumero(barraResultado.value);
+            switch(operador){
+                case '+':
+                    resultado = terminosOperacion[0] + terminosOperacion[1];
+                    break;
+                case '-':
+                    resultado = terminosOperacion[0] - terminosOperacion[1];
+                    break;
+                case '/':
+                    resultado = terminosOperacion[0] / terminosOperacion[1];
+                    break;
+                case '*':
+                    resultado = terminosOperacion[0] * terminosOperacion[1];
+                    break;
+                default:
+                    alert('Operacion no valida');
+                    break;
+            }  
+            barraResultado.value = resultado;
+            terminosOperacion.length = 0;
+            operador = '';
+            numero = 0;
+        }
+        else{
+            document.getElementById('btnLimpiar').click();
         }
     });
 });
+
+function construirNumero(numeroString){
+    return Number(numeroString);
+}
